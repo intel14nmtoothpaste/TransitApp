4417e7490319b7e7f873cf1ff7892b3d7181167b
intel 14nm toothpaste <henrylamhs@gmail.com>
2025-05-02 16:01:07 +0800

    Initial Commit

---
+++ TransitApp/TransitAppApp.swift
@@ -0,0 +1,32 @@
+//
+//  TransitAppApp.swift
+//  TransitApp
+//
+//  Created by Henry Lam on 2/5/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct TransitAppApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ TransitAppTests/TransitAppTests.swift
@@ -0,0 +1,17 @@
+//
+//  TransitAppTests.swift
+//  TransitAppTests
+//
+//  Created by Henry Lam on 2/5/2025.
+//
+
+import Testing
+@testable import TransitApp
+
+struct TransitAppTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
